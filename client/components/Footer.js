c13137169f17bd3e3604e0e4f0033ead02e4783e
Arman Siddiqui <arman@golokait.com>
2024-12-10 13:15:44 +0530

    second

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full  py-4 text-center">
-      <p className="text-center text-gray-600">Assam Engineering College</p>
+      <p className="text-center text-gray-600">By Students of</p>
+      <p className="text-center text-gray-600">Ajay Kumar Garg Engineering College</p>
     </footer>
   );
 };
